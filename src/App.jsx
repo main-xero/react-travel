9d36dcb3ec4d34d6fc03a111e5fa5c0ebd4f1f81
cbmastery <vassil.kurk@gmail.com>
2024-12-06 18:53:53 -0500

    final

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ import data from "./data";
 
 export default function App() {
   const entryElements = data.map((entry) => {
-    return <Destinations key={entry.id} entry={entry} />;
+    return (
+      <>
+        <Destinations key={entry.id} entry={entry} />
+      </>
+    );
   });
 
   return (
